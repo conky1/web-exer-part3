4caf28f0cb7fbc3e02fc5f2494fb447bcaeb76c9
Petri Sipinen <petri.sipinen@centria.fi>
2021-06-08 22:51:33 +0300

    8.6.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
+
 let notes = [
     {
       id: 1,
@@ -29,6 +32,30 @@ app.get('/', (req, res) => {
 app.get('/api/notes', (req, res) => {
   res.json(notes)
 })
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+
+    if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+
+    app.post('/api/notes', (request, response) => {
+        const note = request.body
+        console.log(note)
+        response.json(note)
+      })
+
+    app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+    
+    response.status(204).end()
+    })
 
 const PORT = 3001
 app.listen(PORT, () => {
