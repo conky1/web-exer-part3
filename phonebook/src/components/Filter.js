2602353085331fda08a2ed2827864e44f9e2eb29
Petri Sipinen <petri.sipinen@centria.fi>
2021-06-11 01:49:35 +0300

    10.6.

--- phonebook/src/components/Filter.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Filter = ({ filterValue, filterOnChange }) => {
-    return (
-      <>filter shown with: <input value={filterValue} onChange={filterOnChange} /><br /></>
-    )
-}
-
-export default Filter
